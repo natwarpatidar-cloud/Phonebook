28d60d3d45914db1d16481b0259d84be761e6709
NatwarPatidar <natwar.patidar@gammaedge.io>
2025-10-23 15:29:16 +0530

    add validation for phone number

--- client/src/components/Modal/ContactModal.jsx
+++ client/src/components/Modal/ContactModal.jsx
@@ -63,10 +63,10 @@ export default function ContactModal({ isOpen, onClose, title, buttonText, data,
 
     if (!formData.name.trim()) newErrors.name = 'Name is required';
     if (!formData.phone.trim()) newErrors.phone = 'Phone number is required';
+    if(formData.phone.length !== 10) newErrors.phone = 'Phone number should be of 10 digits'
     if (!formData.address.trim()) newErrors.address = 'Address is required';
     if (!formData.label.trim()) newErrors.label = 'Tag is required';
     // if (!formData.avatar) newErrors.avatar = 'Avatar is required';
-
     setErrors(newErrors);
     return Object.keys(newErrors).length === 0;
   };
